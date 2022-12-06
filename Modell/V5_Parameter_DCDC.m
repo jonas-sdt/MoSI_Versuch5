d72f1670300e383227c68536b8c22d08259cade4
fls3661 <fls3661@thi.de>
2022-12-06 13:14:36 +0100

    Parameter File geupdated

--- Modell/V5_Parameter_DCDC.m
+++ Modell/V5_Parameter_DCDC.m
@@ -1,3 +1,8 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Parameterfile für MoSi Versuch 5 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+L = 10e-3; % [H] Induktivität der Spule
+R_L = 19; % [ohm] Wicklungswiderstand der Spule
+C = 100e-9; % [F] Kapazität des Kondensators
+R_Last = 1;    % [ohm] Lastwiderstand
