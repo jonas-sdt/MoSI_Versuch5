0aa9a573c6eb4253f05f10fb0e195b5dedfef352
fls3661 <fls3661@thi.de>
2022-12-08 13:50:36 +0100

    Parameter Update

--- Modell/V5_Parameter_DCDC.m
+++ Modell/V5_Parameter_DCDC.m
@@ -6,3 +6,19 @@ L = 10e-3; % [H] Induktivität der Spule
 R_L = 19; % [ohm] Wicklungswiderstand der Spule
 C = 100e-9; % [F] Kapazität des Kondensators
 R_Last = 1;    % [ohm] Lastwiderstand
+
+U_e; % [V] Eingangs- bzw. Versorgungsspannung
+i_e; % [A] Eingangsstrom
+
+d; % Schaltgröße d
+
+U = d* U_e; % [V] Eingangsgröße Ersatzschaltbild
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ERWEITERUNG DES MODELLS UM VERLUSTMECHANISMEN %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+R_VD; % [ohm] Durchlassverluste
+R_VS  % [ohm] Sperrverluste
+
+R_V = d *R_VD + (1-d)*R_VS; % [ohm]Verlustwiderstand
