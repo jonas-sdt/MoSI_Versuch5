390fe3f122dccaca59471e47ed59053c7275b4f5
Jonas Schmidt <jonas.schmidt@schanzer-racing.de>
2022-12-08 21:24:57 +0100

    Parameterfile formattiert

--- Modell/V5_Parameter_DCDC.m
+++ Modell/V5_Parameter_DCDC.m
@@ -2,23 +2,20 @@
 % Parameterfile für MoSi Versuch 5 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-L = 10e-3; % [H] Induktivität der Spule
-R_L = 19; % [ohm] Wicklungswiderstand der Spule
-C = 100e-9; % [F] Kapazität des Kondensators
-R_Last = 1;    % [ohm] Lastwiderstand
+% Parameter Bauteile
+    L = 10e-3;      % [H]   Induktivität der Spule
+    R_L = 19;       % [ohm] Wicklungswiderstand der Spule
+    C = 100e-9;     % [F]   Kapazität des Kondensators
+    R_Last = 1;     % [ohm] Lastwiderstand
+    
+% Parameter zu Spannungsversorgung
+    U_e;            % [V]   Eingangs- bzw. Versorgungsspannung
+    i_e;            % [A]   Eingangsstrom
+    d;              %       Schaltgröße d
+    U = d* U_e;     % [V]   Eingangsgröße Ersatzschaltbild
 
-U_e; % [V] Eingangs- bzw. Versorgungsspannung
-i_e; % [A] Eingangsstrom
-
-d; % Schaltgröße d
-
-U = d* U_e; % [V] Eingangsgröße Ersatzschaltbild
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% ERWEITERUNG DES MODELLS UM VERLUSTMECHANISMEN %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-R_VD; % [ohm] Durchlassverluste
-R_VS  % [ohm] Sperrverluste
-
-R_V = d *R_VD + (1-d)*R_VS; % [ohm]Verlustwiderstand
+% Erweiterung um Verlustmechanismen
+    R_VD;           % [ohm] Durchlassverluste
+    R_VS            % [ohm] Sperrverluste
+    
+    R_V = d*R_VD + (1-d)*R_VS; % [ohm]Verlustwiderstand
