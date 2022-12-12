fde36aa419d030402d87a67c871caefbac061b8a
fls3661 <fls3661@thi.de>
2022-12-12 10:00:42 +0100

    Parameter File Update

--- Modell/V5_Parameter_DCDC.m
+++ Modell/V5_Parameter_DCDC.m
@@ -29,12 +29,15 @@
     R_V = d*R_VD + (1-d)*R_VS;  % [ohm]Verlustwiderstand
 
 % R_Last
-    % Ausgangsspannungen gemäß Tabelle 5.7 Spalte 2, zweite Zeile des Arrays Widerstandswerte gemäß 5.7 Spalte 3
-    KL_R_Last_1B = [0.641 1.511 2.296 3.161 4.891 6.688 10.194 8.453 ; 40.095 57.51 67.735 76.625 90.313 101.71 119.017 110.606]; 
+    % Ausgangsspannungen gemäß Tabelle 5.7 Spalte 2. zweite Zeile des Arrays Widerstandswerte gemäß 5.7 Spalte 3
+    % KL_R_Last_1B = [0.641 1.511 2.296 3.161 4.891 6.688 10.194 18.453 ; 40.095 57.51 67.735 76.625 90.313 101.71 119.017 110.606];
+    KL_R_Last_1B = [0.641	1.511	2.296	3.161	4.891	6.688	8.453	10.194; 
+                    40.095	57.51	67.735	76.625	90.313	101.71	110.606	119.017];
+
     % [ohm] Widerstand R_Last Fall 1
 
-    % Ausgangsspannungen gemäß Tabelle 5.8 Spalte 2, zweite Zeile des Arrays Widerstandswerte gemäß 5.8 Spalte 3
-    KL_R_Last_2B = [0.397 1.066 1.717 2.435 3.902 5.434 7.969 6.929  ; 13.571 22.508 27.963 31.963 38.785 44.369 51.675 48.864];  
+    % Ausgangsspannungen gemäß Tabelle 5.8 Spalte 2. zweite Zeile des Arrays Widerstandswerte gemäß 5.8 Spalte 3
+    KL_R_Last_2B = [0.397 1.066 1.717 2.435 3.902 5.434 7.969 16.929  ; 13.571 22.508 27.963 31.963 38.785 44.369 51.675 48.864];  
     % [ohm] Widerstand R_Last Fall 2
 
     B = 1; % Umschaltung zwischen beiden Kennlinien 1 & 2
